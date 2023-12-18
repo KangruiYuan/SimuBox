e5a00415690bdf23e469a413077136e41b111b50
Alkaid <1176671796@qq.com>
2023-12-18 20:56:03 +0800

    fix web

--- SimuBox/Web/web_unit.py
+++ SimuBox/Web/web_unit.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import streamlit as st
 import base64
 
-logo = f"url(data:image/png;base64,{base64.b64encode((st.session_state.current_dir / 'SimuBox.png').read_bytes()).decode()})"
 
 
 def check_state(**kwargs):
@@ -11,7 +10,7 @@ def check_state(**kwargs):
         st.session_state.save_auto = kwargs.get("save_auto", False)
 
     if "current_dir" not in st.session_state:
-        st.session_state.current_dir = Path(__file__).parent
+        st.session_state["current_dir"] = Path(__file__).parent
 
     if "cache_dir" not in st.session_state:
         st.session_state.cache_dir = st.session_state.current_dir / kwargs.get(
@@ -24,6 +23,10 @@ def check_state(**kwargs):
     if "dpi" not in st.session_state:
         st.session_state.dpi = kwargs.get("dpi", 150)
 
+    if "logo" not in st.session_state:
+        logo_path = st.session_state.current_dir / 'SimuBox.png'
+        st.session_state.logo = f"url(data:image/png;base64,{base64.b64encode(logo_path.read_bytes()).decode()})"
+
 
 def add_logo(**kwargs):
 
@@ -31,7 +34,7 @@ def add_logo(**kwargs):
         f"""
         <style>
             [data-testid="stSidebarNav"] {{
-                background-image: {logo};
+                background-image: {st.session_state.logo};
                 background-repeat: no-repeat;
                 padding-top: 0px;
                 background-position: 20px 50px;
